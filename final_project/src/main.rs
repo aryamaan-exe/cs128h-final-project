c4ea8c1e75c895e5f947100a366bc1c28d4eb364
sabona-abdi <sabona.abdi@senecafoundation.org>
2026-04-12 15:33:03 -0500

    creating initial wireshark CLI skeleton \npacket capture not yet implemented \nable to list device internet interfaces and capture cli args

--- final_project/src/main.rs
+++ final_project/src/main.rs
@@ -1,13 +1,52 @@
-// This function should take in and out something
-fn encrypt() {
-    todo!()
-}
-
-// This function should take in and out something
-fn decrypt() {
-    todo!()
+use clap::Parser;
+use pcap::Device;
+#[derive(Parser, Debug)]
+struct Args {
+    // network interface to capture on: ethernet, wifi card, etc.
+    #[arg(short, long)]
+    interface: Option<String>,
+    // number of packets to capture
+    #[arg(short, long, default_value_t = 100)]
+    count: usize,
+    // used to print available interface
+    #[arg(long)]
+    list: bool
 }
 
 fn main() {
-    println!("Hello, world!");
+    let cli_arguments: Args = Args::parse();
+    // if --list flag is utilized
+    if cli_arguments.list {
+        let device_interface_list = match Device::list() {
+            Ok(vector) => vector,
+            Err(e) => {
+                println!("The following error was arose trying to generate a custom device interface list: \n{}", e);
+                return;
+            }
+        };
+        println!("List of custom device interfaces: ");
+        for device_interface in &device_interface_list {
+            println!("{}", device_interface.name);
+        }
+        println!("Use one of the interfaces above with the --interface flag to capture packets on that interface.");
+    } else {
+        // if --list flag is NOT utilized
+        match cli_arguments.interface {
+            Some(val) => println!("Interface: {}", val),
+            None => {
+                println!("No flags utilized, please reference RUN.md for specifications");
+            }
+        } 
+    }  
 }
+
+
+// This function should take in and out something
+// fn encrypt() {
+//     todo!()
+// }
+
+// This function should take in and out something
+// fn decrypt() {
+//     todo!()
+// }
