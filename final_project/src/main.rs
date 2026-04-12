86acc6e31988f8e9eaa0f458779c700fe4c2810b
Aidan Kim <aidankim@Aidans-MacBook-Pro-7.local>
2026-04-11 19:10:02 -0500

    Added tun

--- final_project/src/main.rs
+++ final_project/src/main.rs
@@ -1,3 +1,13 @@
+// This function should take in and out something
+fn encrypt() {
+    todo!()
+}
+
+// This function should take in and out something
+fn decrypt() {
+    todo!()
+}
+
 fn main() {
     println!("Hello, world!");
 }
